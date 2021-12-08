1c8d4334057933084e8b5075c89c7d0fb97ac9d2
Christoph Mellüh <christoph@mellueh.de>
2021-12-08 12:53:11 +0100

    make nodes movable

--- nodes.py
+++ nodes.py
@@ -10,10 +10,113 @@ from PyQt5.QtWidgets import QApplication,QGraphicsView, QGraphicsScene, QGraphic
 from PyQt5.QtCore import Qt, QPointF
 
 
+class connector(QtGui.QPainterPath):
+
+    def __init__(self,x1,y1,x2,y2):
+        super().__init__()
+
+        self.x1 = x1
+        self.x2=x2
+        self.y1=y1
+        self.y2=y2
+
+    pass
+
+
+class drag_box(QtWidgets.QGraphicsProxyWidget):
+
+    def __init__(self,widget):
+        super().__init__()
+        self.setAcceptHoverEvents(True)
+        self.setWidget(widget)
+        self.connected_entities=[]
+
+
+    def connect_to_entity(self,entity):
+
+        if entity is not None:
+            self.connected_entities.append(entity)
+
+            scene = self.scene()
+            rect1 = self.windowFrameGeometry()
+            rect2 = entity.windowFrameGeometry()
+
+
+            print(rect1)
+            print(rect2)
+
+
+            p1 = QPointF()
+            p2 = QPointF()
+            p1.setX(rect1.left())
+            p1.setY(rect1.top()+rect1.height()/2)
+
+
+
+            p2.setX(rect2.right())
+            p2.setY(rect2.top()+rect2.height()/2)
+
+
+
+
+            path = QtGui.QPainterPath()
+            path.moveTo(p1)
+            path.lineTo(p2)
+            scene.addPath(path)
+
+    def hoverEnterEvent(self,event):
+        app.instance().setOverrideCursor(QtCore.Qt.OpenHandCursor)
+
+    def hoverLeaveEvent(self,event):
+        app.instance().restoreOverrideCursor()
+
+    def mousePressEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
+        rect1 = self.windowFrameRect()
+        print(rect1)
+
+        # rect = self.windowFrameGeometry()
+        #
+        # center_left = (rect.bottomLeft()-rect.topLeft())/2
+        # scene = self.scene()
+        #
+        # print(rect)
+        # print(center_left)
+        #
+        # path = QtGui.QPainterPath()
+        # path.currentPosition()
+        # p1 = QPointF()
+        # p2 = QPointF()
+        # p1.setX(rect.left())
+        # p1.setY(rect.top()+rect.height()/2)
+        #
+        # path.lineTo(p1)
+        # scene.addPath(path)
+
+        pass
+
+    def mouseMoveEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
+        orig_cursor_position = event.lastScenePos()
+        updated_cursor_position = event.scenePos()
+
+        orig_position = self.scenePos()
+        updated_cursor_x = updated_cursor_position.x() - orig_cursor_position.x()+orig_position.x()
+        updated_cursor_y = updated_cursor_position.y() - orig_cursor_position.y()+orig_position.y()
+
+        self.setPos(QPointF(updated_cursor_x,updated_cursor_y))
+
+
+
+        pass
+
+    def mouseReleaseEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
+            pass
+
+    pass
 
 class EntityRepresentation(QGroupBox):
-    def __init__(self,x):
-        super().__init__(x)
+
+    def __init__(self,):
+        super().__init__()
         self.attrib_list = None
 
     def add_attribute(self,text,data):
@@ -22,43 +125,74 @@ class EntityRepresentation(QGroupBox):
         attrib.setText(text)
 
 
-
-
-
-
-
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow,app):
 
+
+        ### Fenster Aufbau
+
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1527, 920)
+        MainWindow.resize(1920, 1080)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
+
+        self.base_layout = QtWidgets.QGridLayout(self.centralwidget)
+        self.base_layout.setObjectName("baseLayout")
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setSpacing(5)
         self.horizontalLayout.setObjectName("horizontalLayout")
+
         self.treeWidget = QtWidgets.QTreeWidget(self.centralwidget)
+
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.treeWidget.sizePolicy().hasHeightForWidth())
+
         self.treeWidget.setSizePolicy(sizePolicy)
         self.treeWidget.setMaximumSize(QtCore.QSize(14000, 16777215))
-        self.treeWidget.setObjectName("treeWidget")
+        self.treeWidget.setObjectName("RuleBrowser")
         self.treeWidget.headerItem().setText(0, "1")
         self.horizontalLayout.addWidget(self.treeWidget)
-        self.rule_view = QtWidgets.QGroupBox(self.centralwidget)
+
+        self.rule_view = QtWidgets.QGroupBox()
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(3)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.rule_view.sizePolicy().hasHeightForWidth())
         self.rule_view.setSizePolicy(sizePolicy)
         self.rule_view.setObjectName("rule_view")
+
+        self.rule_view_grid = QtWidgets.QGridLayout(self.rule_view)
+        self.rule_view_grid.setObjectName("rule_view_grid")
+
+
+        self.scene = QGraphicsScene()
+        self.graphicsView = QtWidgets.QGraphicsView(self.scene)
+        self.graphicsView.setObjectName("graphicsView")
+        self.rule_view_grid.addWidget(self.graphicsView, 0, 0, 1, 1)
+
+
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
+        self.graphicsView.setSizePolicy(sizePolicy)
+
+
+
+        #print(self.graphicsView.width())
+
+
+
+
+
+
+
         self.horizontalLayout.addWidget(self.rule_view)
-        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.base_layout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1527, 22))
@@ -71,8 +205,8 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
         self.initialize()
+
 
 
 
@@ -83,17 +217,20 @@ class Ui_MainWindow(object):
 
 
     def initialize(self):
+        MainWindow.show()
+
+        width, height = self.rule_view.width()-50, self.rule_view.height()-50
+
+        self.scene.setSceneRect(0, 0, width, height)
 
         self.init_tree()
         self.entity_list = []
-
+        print(self.rule_view.width())
 
         pass
 
     def init_tree(self):
         self.treeWidget.setColumnCount(1)
-
-
 
         for concept_root in ConceptRoot.instances():
             item = QTreeWidgetItem([concept_root.has_for_name])
@@ -105,6 +242,7 @@ class Ui_MainWindow(object):
                 child.data = concept
                 item.addChild(child)
 
+
         self.treeWidget.itemClicked.connect(self.on_tree_clicked)
 
 
@@ -112,6 +250,8 @@ class Ui_MainWindow(object):
 
         obj = item.data
         for el in self.entity_list:
+
+
             el.setParent(None)
 
         self.entity_list= []
@@ -134,15 +274,16 @@ class Ui_MainWindow(object):
         path = paths[-1]
 
         entity = None
+        proxy = None
 
         for element in path:
             if(ConceptTemplate.__instancecheck__(element)):
 
-                entity = self.add_entity(element.has_for_applicable_entity,element)
+                proxy,entity = self.add_entity(element.has_for_applicable_entity,element,proxy)
 
                 pass
             elif(EntityRule.__instancecheck__(element)):
-                entity = self.add_entity(element.has_for_entity_name,element)
+                proxy,entity = self.add_entity(element.has_for_entity_name,element,proxy)
                 pass
             elif(AttributeRule.__instancecheck__(element)):
 
@@ -150,32 +291,39 @@ class Ui_MainWindow(object):
 
 
             else:
-                self.add_label(element)
+                self.add_label(element,proxy)
                 pass
 
 
-    def add_entity(self,name,data):
-
-
-
-
+    def add_entity(self,name,data,old_proxy):
 
         el = self.entity_list
 
-        entity =EntityRepresentation(self.rule_view)
+        entity =EntityRepresentation()
+
+        proxy = drag_box(entity)
+
+
+
+
+
+
+        self.scene.addItem(proxy)
+
+
 
         width = 200
-        heigth = 75
+        heigth = 50
 
         xpos = 25+len(el)*(width+25)
         ypos = 25
 
-
-
-        entity.setGeometry(QtCore.QRect(xpos, ypos, width, heigth))
+        proxy.setPos((QPointF(xpos,ypos)))
+        proxy.resize(width,heigth)
         entity.setObjectName(name)
         entity.setTitle(name)
 
+        proxy.connect_to_entity(old_proxy)
 
         grid = QtWidgets.QGridLayout(entity)
         grid.setObjectName("internes_grid")
@@ -185,25 +333,22 @@ class Ui_MainWindow(object):
 
 
         entity.show()
-
         self.entity_list.append(entity)
+        return proxy,entity
 
 
-
-
-
-        return entity
-
-
-    def add_label(self,inhalt):
+    def add_label(self,inhalt,old_proxy):
 
         text = str(inhalt)
 
         entity_list = self.entity_list
 
 
-        label = QtWidgets.QLabel(self.rule_view)
+        label = QtWidgets.QLabel()
+        proxy = drag_box(label)
 
+
+        self.scene.addItem(proxy)
         label.setText(text)
         width = 200
         height = 50
@@ -213,9 +358,10 @@ class Ui_MainWindow(object):
 
 
         label.setGeometry(xpos,ypos,width,height)
+        proxy.show()
         label.show()
 
-
+        proxy.connect_to_entity(old_proxy)
 
         if bool.__instancecheck__(inhalt):
             if inhalt == True:
@@ -233,6 +379,7 @@ class Ui_MainWindow(object):
 
         entity_list.append(label)
 
+        return proxy,label
 
 
 
@@ -257,6 +404,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow,app)
-    MainWindow.show()
+    #MainWindow.show()
     sys.exit(app.exec_())
 
