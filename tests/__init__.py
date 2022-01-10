5ac5b61b00566a285f6db074c73e211fb1a09ff9
krapottke1312 <74136980+krapottke1312@users.noreply.github.com>
2022-01-10 23:35:46 +0100

    small fixes

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+'''
+This File is for Testing the conversion from MVD to Onthology
+
+Classes:
+
+    None
+
+Functions:
+
+    main()
+
+Misc variables:
+
+    None
+'''
+
+import test
+test.main()
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from mvd2onto.classes import *
+from mvd2onto.core import *
 '''
 This File is for Testing the conversion from MVD to Onthology
 
@@ -69,4 +69,5 @@ def main():
         print(ct.has_for_name+ct.has_for_uuid)
 
 if __name__ == "__main__":
+
     main()
