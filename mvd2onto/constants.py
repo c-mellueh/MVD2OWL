27039ca731f2ca985f96fbcaa15d32b52f7c2df0
krapottke1312 <74136980+krapottke1312@users.noreply.github.com>
2022-03-01 14:44:17 +0100

    add constants.py

---
+++ mvd2onto/constants.py
@@ -0,0 +1,16 @@
+"""constants for visualization"""
+
+
+# text constants
+
+UNDEFINED_ROOT_NAME = "undefined"
+
+
+
+# geometrical constants
+
+BORDER = 5
+RESIZE_BORDER_WIDTH = 10
+SCALING_FACTOR =0.1
+
+
